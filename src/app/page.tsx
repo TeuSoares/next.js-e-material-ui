6625c17d45579d0805f87c3b1e4e561a546cb6a2
TeuSoares <mateussoares114@gmail.com>
2023-08-14 17:55:43 -0300

    14/08/2023

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import CrmButton from '@/components/buttons/CrmButton';
+import CrmInputDate from '@/components/form/components/inputs/CrmInputDate';
+import CrmInputText from '@/components/form/components/inputs/CrmInputText';
 import CrmForm from '@/components/form/CrmForm';
-import CrmInputDate from '@/components/inputs/CrmInputDate';
-import CrmInputText from '@/components/inputs/CrmInputText';
 import CrmBase from '@/components/layout/CrmBase';
 import CrmContainer from '@/components/layout/CrmContainer';
+import CrmLoading from '@/components/layout/CrmLoading';
 import CrmTable from '@/components/table/CrmTable';
+
+import PortalServices from '@/services/PortalServices';
 
 import DeleteIcon from '@mui/icons-material/Delete';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
@@ -16,13 +19,13 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 export default function Home() {
-  const handleSearchDuplicata = (data: object) => {
-    console.log(data);
-  };
-
-  const handleFiltersDuplicatas = (data: object) => {
-    console.log(data);
-  };
+  const {
+    data,
+    error,
+    isLoading,
+    handleFilterDuplicates,
+    handleSearchDuplicate
+  } = PortalServices();
 
   return (
     <CrmContainer>
@@ -39,7 +42,7 @@ export default function Home() {
             <Typography variant="overline" fontWeight="bold" mb={1}>
               Filtrar por períodos de vencimento
             </Typography>
-            <CrmForm onSubmit={handleFiltersDuplicatas}>
+            <CrmForm onSubmit={handleFilterDuplicates}>
               <CrmInputDate
                 label="Data Inicial"
                 name="dataInicial"
@@ -59,20 +62,14 @@ export default function Home() {
                   marginBottom: { xs: '1em', sm: '0' }
                 }}
               />
-              <CrmButton
-                text={<FilterAltIcon />}
-                type="submit"
-                customCss={{
-                  width: { xs: '100%', sm: 'auto' }
-                }}
-              />
+              <CrmButton text={<FilterAltIcon />} type="submit" />
             </CrmForm>
           </Box>
           <Box width={{ mid: '35%' }} mb={{ xs: 2, mid: 0 }}>
             <Typography variant="overline" fontWeight="bold" mb={1}>
               Pesquisar por duplicata
             </Typography>
-            <CrmForm onSubmit={handleSearchDuplicata}>
+            <CrmForm onSubmit={handleSearchDuplicate}>
               <Box sx={{ paddingTop: '8px' }}>
                 <CrmInputText
                   name="codigo"
@@ -84,13 +81,7 @@ export default function Home() {
                     marginRight: { sm: '1em' }
                   }}
                 />
-                <CrmButton
-                  text={<SearchIcon />}
-                  type="submit"
-                  customCss={{
-                    width: { xs: '100%', sm: 'auto' }
-                  }}
-                />
+                <CrmButton text={<SearchIcon />} type="submit" />
               </Box>
             </CrmForm>
           </Box>
@@ -105,14 +96,15 @@ export default function Home() {
               startIcon={<DeleteIcon />}
               color="red"
               customCss={{
-                width: { xs: '100%', sm: 'auto' },
                 marginTop: { xs: '1em', sm: '0' }
               }}
             />
           </Box>
         </Stack>
         <Box>
-          <CrmTable />
+          {isLoading && <CrmLoading />}
+          {error && <span>Houve algum erro</span>}
+          {data && <CrmTable rows={data} />}
         </Box>
       </CrmBase>
     </CrmContainer>
